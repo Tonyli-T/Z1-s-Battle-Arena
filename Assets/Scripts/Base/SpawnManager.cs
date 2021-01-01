66470e55f3cb722ec9b6e8e16c74704e46b64b02
Yizhou Li <lyzpp2000@163.com>
2021-01-01 17:41:07 -0500

    Finishing Spawn phase1.

---
+++ Assets/Scripts/Base/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject creep;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 5, 10);       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // To spawn creeps
+    private void Spawn()
+	{
+		Instantiate(creep, transform.position + new Vector3(3, 0, 0), transform.rotation);
+        Instantiate(creep, transform.position + new Vector3(0, 3, 0), transform.rotation);
+        Instantiate(creep, transform.position - new Vector3(0, 3, 0), transform.rotation);
+    }
+}
