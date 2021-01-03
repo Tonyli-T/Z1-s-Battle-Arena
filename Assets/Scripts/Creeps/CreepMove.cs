40bf51a2b32ef4d1a955d5f5a01150167f61397f
Yizhou Li <lyzpp2000@163.com>
2021-01-03 12:26:19 -0500

    Working on creep path.

--- Assets/Scripts/Creeps/CreepMove.cs
+++ Assets/Scripts/Creeps/CreepMove.cs
@@ -1,35 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AI;
+﻿using UnityEngine;
 
 public class CreepMove : MonoBehaviour
 {
-    public Transform goal;
-    private Rigidbody2D rb2D;
-    public float speed = 5;
-    public float damage = 5;
+	public GameObject enemyBase;
+	private Rigidbody2D rb2D;
+	public float speed = 5;
+	public float damage = 5;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb2D = GetComponent<Rigidbody2D>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        var moveTowards = goal.position - transform.position;
-        rb2D.AddForce(moveTowards.normalized * speed, ForceMode2D.Force);
-    }
-
-	private void OnTriggerEnter2D(Collider2D collision)
+	// Start is called before the first frame update
+	void Start()
 	{
-        rb2D.velocity = new Vector2(0, 0);
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		var moveTowards = enemyBase.transform.position - transform.position;
+		rb2D.AddForce(moveTowards.normalized * speed, ForceMode2D.Force);
+	}
+
+	private void OnTriggerStay2D(Collider2D collision)
+	{
+		rb2D.velocity = new Vector2(0, 0);
 
 		if (collision.CompareTag("Enemy Building"))
 		{
-            collision.GetComponent<BaseStats>().currentHealth -= damage;
-        }
-    }
+			collision.GetComponent<BaseStats>().currentHealth -= damage * Time.deltaTime;
+		}
+	}
 }
