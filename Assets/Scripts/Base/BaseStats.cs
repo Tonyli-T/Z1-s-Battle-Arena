c52f8107a2dd57dfd2b533e790be84d3f813e283
Yizhou Li <lyzpp2000@163.com>
2021-01-05 11:18:26 -0500

    更新小兵路径（最新）

--- Assets/Scripts/Base/BaseStats.cs
+++ Assets/Scripts/Base/BaseStats.cs
@@ -3,7 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseStats : MonoBehaviour
-{
+{  
     public float maxHealth;
     public float currentHealth;
+
+	private void Update()
+	{
+		if (currentHealth <= 0)
+		{
+			GameObject.Destroy(gameObject);
+		}
+	}
 }
+ 
--- Assets/Scripts/Base/SpawnManager.cs
+++ Assets/Scripts/Base/SpawnManager.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject creep;
+    public GameObject creepUp;
+    public GameObject creepMid;
+    public GameObject creepDown;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("Spawn", 5, 5);       
+        InvokeRepeating("Spawn", 1, 5);       
     }
 
     // Update is called once per frame
@@ -21,8 +23,16 @@ public class SpawnManager : MonoBehaviour
     // To spawn creeps
     private void Spawn()
 	{
-		Instantiate(creep, transform.position + new Vector3(3, 0, 0), transform.rotation);
-        Instantiate(creep, transform.position + new Vector3(0, 3, 0), transform.rotation);
-        Instantiate(creep, transform.position - new Vector3(0, 3, 0), transform.rotation);
+		if (gameObject.CompareTag("太阳圣殿"))
+		{
+            Instantiate(creepMid, transform.position + new Vector3(3, 0, 0), transform.rotation);
+        }
+        else if (gameObject.CompareTag("奥姆真理"))
+        {
+            Instantiate(creepMid, transform.position - new Vector3(3, 0, 0), transform.rotation);
+        }
+
+        Instantiate(creepUp, transform.position + new Vector3(0, 3, 0), transform.rotation);
+        Instantiate(creepDown, transform.position - new Vector3(0, 3, 0), transform.rotation);
     }
 }
