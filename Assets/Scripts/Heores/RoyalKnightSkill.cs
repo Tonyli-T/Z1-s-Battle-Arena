aff9deaa0be0056521d7b1cf8936c41b7fdb8d0e
Yizhou Li <lyzpp2000@163.com>
2021-01-13 18:37:31 -0500

    Making skills.

--- Assets/Scripts/Heores/RoyalKnightSkill.cs
+++ Assets/Scripts/Heores/RoyalKnightSkill.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class RoyalKnightSkill : MonoBehaviour
 {
-	public bool allowAttack;
-
 	public bool allowQ;
 	public float startTimeQ;
 	public float currentTimeQ;
@@ -26,12 +24,12 @@ public class RoyalKnightSkill : MonoBehaviour
 	
 	private void Update()
 	{
-		attack();
-		cast();
+		Attack();
+		Cast_Spell();
 	}
 	
 	// The method used to cast spells
-	void cast()
+	void Cast_Spell()
 	{
 		// Pressing the keys
 		if (Input.GetKeyDown(KeyCode.Q) && allowQ)
@@ -40,14 +38,14 @@ public class RoyalKnightSkill : MonoBehaviour
 			gameObject.GetComponent<Stats>().armor = 100;
 
 			allowQ = false;
-			startTimeQ = Time.realtimeSinceStartup;
+			startTimeQ = Time.time;
 		}
 		if (Input.GetKeyDown(KeyCode.W) && allowW)
 		{
 			gameObject.transform.position += new Vector3(5, 0, 0);
 
 			allowW = false;
-			startTimeW = Time.realtimeSinceStartup;
+			startTimeW = Time.time;
 		}
 		if (Input.GetKeyDown(KeyCode.E) && allowE)
 		{
@@ -57,46 +55,46 @@ public class RoyalKnightSkill : MonoBehaviour
 
 			allowE = false;
 			lockE = true;
-			startTimeE = Time.realtimeSinceStartup;
+			startTimeE = Time.time;
 		}
 
 		// Controlling the freeze time for Q
-		if (allowQ == false && Time.realtimeSinceStartup - startTimeQ >= 1)
+		if (allowQ == false && Time.time - startTimeQ >= 1)
 		{
 			gameObject.GetComponent<Stats>().magicResist = 10;
 			gameObject.GetComponent<Stats>().armor = 20;
 		}
-		if (allowQ == false && Time.realtimeSinceStartup - startTimeQ >= 5)
+		else if (allowQ == false && Time.time - startTimeQ >= 5)
 		{
 			allowQ = true;
 		}
 
 		// Controlling the freeze time for W
-		if (allowW == false && Time.realtimeSinceStartup - startTimeW >= 10)
+		if (allowW == false && Time.time - startTimeW >= 10)
 		{
 			allowW = true;
 		}
 
 		// Controlling the freeze time for E
-		if (allowE == false && Time.realtimeSinceStartup - startTimeE >= 5 && lockE)
+		if (allowE == false && Time.time - startTimeE >= 5 && lockE)
 		{
 			gameObject.GetComponent<Stats>().health -= 200;
 			gameObject.GetComponent<Stats>().damage -= 200;
 			gameObject.transform.localScale -= new Vector3(1, 1, 0);
 			lockE = false;
 		}
-		if (allowE == false && Time.realtimeSinceStartup - startTimeE >= 30)
+		else if (allowE == false && Time.time - startTimeE >= 30)
 		{
 			allowE = true;
 		}
 	}
 
 	// Melle attack for Royal Knight
-	void attack()
+	void Attack()
 	{
-		if (Input.GetKeyDown(KeyCode.A) && allowAttack)
+		if (Input.GetKeyDown(KeyCode.A))
 		{
-			allowAttack = false;
+
 		}
 	}
 
